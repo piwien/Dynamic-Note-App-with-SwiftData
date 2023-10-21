c57fbf5f14b099195be0b3935047f6b8f7d03f2b
piwien <berkayyasar01@gmail.com>
2023-10-21 21:40:54 +0300

    Initial Commit

---
+++ Dynamic Note App with SwiftData/Dynamic_Note_App_with_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_Note_App_with_SwiftDataApp.swift
+//  Dynamic Note App with SwiftData
+//
+//  Created by Berkay Yaşar on 21.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_Note_App_with_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
